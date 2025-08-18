c0a0a9cd1ceea6e12b77864a2464ac7d88e4a429
Eyal Haimov <eyalhaimoon@gmail.com>
2025-08-18 15:10:07 +0300

    creating main file in python, added content in readme file about license and file for token

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Bot for telegeram
+# Monitor - bot for telegram
+
+## License
+
+[MIT License](https://opensource.org/licenses/MIT)
---
+++ bot.py
@@ -0,0 +1,9 @@
+import os, telebot
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello, how are you?")
